d088f74667e69c5572f862abbb13b56f9f591a0c
haruto <32rurio@gmail.com>
2022-09-30 15:16:40 +0900

    投稿完了

--- frontend/lib/menu/circulation_board/time_line_page.dart
+++ frontend/lib/menu/circulation_board/time_line_page.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/model/post.dart';
+import 'package:frontend/utils/firestore/post.dart';
+import 'package:frontend/utils/firestore/users.dart';
 import 'package:intl/intl.dart';
 import '../../model/account.dart';
-import 'post_page.dart';
 
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
@@ -13,31 +14,6 @@ class TimeLinePage extends StatefulWidget {
 }
 
 class _TimeLinePageState extends State<TimeLinePage> {
-  Account myAccount = Account(
-      id: '1',
-      name: 'Flutterラボ',
-      selfIntroduction: 'こんばんは',
-      userId: 'flutter_lab',
-      imagePath: 'https://avatars.githubusercontent.com/u/87113276?s=40&v=4',
-      createdTime: Timestamp.now(),
-      updatedTime: Timestamp.now()
-  );
-
-  List<Post> postList = [
-    Post(
-        id: '1',
-        content: '初めまして',
-        postAccountId: '1',
-        createdTime: Timestamp.now()
-    ),
-    Post(
-        id: '2',
-        content: '初めまして2',
-        postAccountId: '1',
-        createdTime: Timestamp.now()
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,59 +23,103 @@ class _TimeLinePageState extends State<TimeLinePage> {
         elevation: 2,
       ),
       body: SizedBox(
-        child: ListView.builder(
-          itemCount: postList.length,
-          itemBuilder: (context, index){
-            return Container(
-              decoration: BoxDecoration(  //一番上の投稿には上下、それ以降は下だけ線で囲う
-                border: index == 0 ? Border(
-                  top: BorderSide(color: Colors.grey, width: 0),
-                  bottom: BorderSide(color: Colors.grey, width: 0),
-                ) : Border(bottom: BorderSide(color: Colors.grey, width: 0),),
-              ),
-              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    radius: 22,
-                    foregroundImage: NetworkImage(myAccount.imagePath),
-                  ),
-                  Expanded(
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start, //左寄り
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            // ↑ 子たちの間に空きスペースを均等に置く。開始/終了にはスペースを設けない。
-                            // 下のRowとTextに適応している。２つしかないため両端に寄る
+        child: StreamBuilder<QuerySnapshot>(
+          stream: PostFireStore.posts.snapshots(),
+          builder: (context, postSnapshot) {
+            if(postSnapshot.hasData){
+              List<String> postAccountIds = [];
+              postSnapshot.data!.docs.forEach((doc) {
+                Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+                if(!postAccountIds.contains(data['post_account_id'])){
+                  postAccountIds.add(data['post_account_id']);
+                }
+              });
+              return FutureBuilder<Map<String, Account>?>(
+                future: UserFirestore.getPostUserMap(postAccountIds),
+                builder: (context, userSnapshot) {
+                  if(userSnapshot.hasData && userSnapshot.connectionState == ConnectionState.done) {
+                    return ListView.builder(
+                      itemCount: postSnapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> data = postSnapshot.data!.docs[index].data() as Map<String, dynamic>;
+                        Post post = Post(
+                          id: postSnapshot.data!.docs[index].id,
+                          content: data['content'],
+                          postAccountId: data['post_account_id'],
+                          createdTime: data['created_time'],
+                        );
+                        Account postAccount = userSnapshot.data![post.postAccountId]!;
+                        return Container(
+                          decoration: BoxDecoration( //一番上の投稿には上下、それ以降は下だけ線で囲う
+                            border: index == 0 ? Border(
+                              top: BorderSide(color: Colors.grey, width: 0),
+                              bottom: BorderSide(color: Colors.grey, width: 0),
+                            ) : Border(bottom: BorderSide(
+                                color: Colors.grey, width: 0),),
+                          ),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 15),
+                          child: Row(
                             children: [
-                              Row(
-                                children: [
-                                  Text(myAccount.name, style: TextStyle(fontWeight: FontWeight.bold),),
-                                  Text(myAccount.userId, style: TextStyle(color: Colors.grey)),
-                                ],
+                              CircleAvatar(
+                                radius: 22,
+                                foregroundImage: NetworkImage(postAccount.imagePath),
                               ),
-                              Text(DateFormat('M/d/yy').format(postList[index].createdTime!.toDate())),
+                              Expanded(
+                                child: Container(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment
+                                        .start, //左寄り
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .spaceBetween,
+                                        // ↑ 子たちの間に空きスペースを均等に置く。開始/終了にはスペースを設けない。
+                                        // 下のRowとTextに適応している。２つしかないため両端に寄る
+                                        children: [
+                                          Row(
+                                            children: [
+                                              Text(postAccount.name,
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight
+                                                        .bold),),
+                                              Text(postAccount.userId,
+                                                  style: TextStyle(
+                                                      color: Colors.grey)),
+                                            ],
+                                          ),
+                                          Text(DateFormat('M/d/yy').format(
+                                              post.createdTime!
+                                                  .toDate())),
+                                        ],
+                                      ),
+                                      Text(post.content),
+                                    ],
+                                  ),
+                                ),
+                              )
                             ],
                           ),
-                          Text(postList[index].content),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            );
-          },
+                        );
+                      },
+                    );
+                  }else{
+                    return Container();
+                  }
+                }
+              );
+          } else {
+              return Container();
+            }
+          }
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => PostPage()));
-          },
-          child: Icon(Icons.chat_bubble_outline)
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //     onPressed: (){
+      //       Navigator.push(context, MaterialPageRoute(builder: (context) => PostPage()));
+      //     },
+      //     child: Icon(Icons.chat_bubble_outline)
+      // ),
     );
   }
 }
