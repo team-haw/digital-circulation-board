b222308dc913f17e327908ec898e392cf3523801
haruto <32rurio@gmail.com>
2022-09-30 14:14:16 +0900

    投稿マイページ

--- frontend/lib/menu/profile2/profile2.dart
+++ frontend/lib/menu/profile2/profile2.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/menu/profile2/edit_profile2.dart';
 import 'package:frontend/utils/authentication.dart';
+import 'package:frontend/utils/firestore/post.dart';
+import 'package:frontend/utils/firestore/users.dart';
 import 'package:intl/intl.dart';
 
 import '../../model/account.dart';
@@ -17,21 +19,6 @@ class AccountPage extends StatefulWidget {
 
 class _AccountPageState extends State<AccountPage> {
   Account myAccount = Authentication.myAccount!;
-
-  List<Post> postList = [
-    Post(
-        id: '1',
-        content: '初めまして',
-        postAccountId: '1',
-        createdTime: Timestamp.now()
-    ),
-    Post(
-        id: '2',
-        content: '初めまして2',
-        postAccountId: '1',
-        createdTime: Timestamp.now()
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -103,53 +90,77 @@ class _AccountPageState extends State<AccountPage> {
                   child: Text('投稿', style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),),
                 ),
                 Expanded(
-                  child: ListView.builder(
-                      physics: NeverScrollableScrollPhysics(), // 投稿エリア、ノースクロール
-                      itemCount: postList.length,
-                      itemBuilder: (context, index){
-                        return Container(
-                          decoration: BoxDecoration(  //一番上の投稿には上下、それ以降は下だけ線で囲う
-                            border: index == 0 ? Border(
-                              top: BorderSide(color: Colors.grey, width: 0),
-                              bottom: BorderSide(color: Colors.grey, width: 0),
-                            ) : Border(bottom: BorderSide(color: Colors.grey, width: 0),),
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                          child: Row(
-                            children: [
-                              CircleAvatar(
-                                radius: 22,
-                                foregroundImage: NetworkImage(myAccount.imagePath),
-                              ),
-                              Expanded(
-                                child: Container(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start, //左寄り
-                                    children: [
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                        // ↑ 子たちの間に空きスペースを均等に置く。開始/終了にはスペースを設けない。
-                                        // 下のRowとTextに適応している。２つしかないため両端に寄る
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: UserFirestore.users.doc(myAccount.id)
+                        .collection('my_posts').orderBy('created_time', descending: true)
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if(snapshot.hasData){
+                        List<String> myPostIds = List.generate(snapshot.data!.docs.length, (index) {
+                          return snapshot.data!.docs[index].id;
+                        });
+                        return FutureBuilder<List<Post>?>(
+                          future: PostFireStore.getPostsFromIds(myPostIds),
+                          builder: (context, snapshot) {
+                            if(snapshot.hasData){
+                              return ListView.builder(
+                                  physics: NeverScrollableScrollPhysics(), // 投稿エリア、ノースクロール
+                                  itemCount: snapshot.data!.length,
+                                  itemBuilder: (context, index){
+                                    Post post = snapshot.data![index];
+                                    return Container(
+                                      decoration: BoxDecoration(  //一番上の投稿には上下、それ以降は下だけ線で囲う
+                                        border: index == 0 ? Border(
+                                          top: BorderSide(color: Colors.grey, width: 0),
+                                          bottom: BorderSide(color: Colors.grey, width: 0),
+                                        ) : Border(bottom: BorderSide(color: Colors.grey, width: 0),),
+                                      ),
+                                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                                      child: Row(
                                         children: [
-                                          Row(
-                                            children: [
-                                              Text(myAccount.name, style: TextStyle(fontWeight: FontWeight.bold),),
-                                              Text(myAccount.userId, style: TextStyle(color: Colors.grey)),
-                                            ],
+                                          CircleAvatar(
+                                            radius: 22,
+                                            foregroundImage: NetworkImage(myAccount.imagePath),
                                           ),
-                                          Text(DateFormat('M/d/yy').format(postList[index].createdTime!.toDate())),
+                                          Expanded(
+                                            child: Container(
+                                              child: Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start, //左寄り
+                                                children: [
+                                                  Row(
+                                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                    // ↑ 子たちの間に空きスペースを均等に置く。開始/終了にはスペースを設けない。
+                                                    // 下のRowとTextに適応している。２つしかないため両端に寄る
+                                                    children: [
+                                                      Row(
+                                                        children: [
+                                                          Text(myAccount.name, style: TextStyle(fontWeight: FontWeight.bold),),
+                                                          Text(myAccount.userId, style: TextStyle(color: Colors.grey)),
+                                                        ],
+                                                      ),
+                                                      Text(DateFormat('M/d/yy').format(post.createdTime!.toDate())),
+                                                    ],
+                                                  ),
+                                                  Text(post.content),
+                                                ],
+                                              ),
+                                            ),
+                                          )
                                         ],
                                       ),
-                                      Text(postList[index].content),
-                                    ],
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
+                                    );
+                                    // ↑ TimeLinePageのタイムライン表示の部分
+                                  }
+                              );
+                            }else{
+                              return Container();
+                            }
+                          }
                         );
-                        // ↑ TimeLinePageのタイムライン表示の部分
+                      }else{
+                        return Container();
                       }
+                    }
                   ),
                 ),
               ],
